f8eca47e230f51b227b5e25b86228e696150eeb2
rovermind <zentlekimbba@gmail.com>
2016-09-02 16:01:28 +0900

    broadcast

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var app = express();
+
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+app.set("port", process.env.PORT || 3030);
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+io.on("connection", function(socket){
+	// console.log("a user connected");
+	// socket.on("disconnect", function(){
+	// 	console.log("a user disconnected");
+	// });
+	socket.on("chat message", function(msg){
+		io.emit("chat message", msg);
+	});
+
+});
+
+http.listen(app.get("port"), function(){
+    console.log("Express chat server is running at localhost:" + app.get("port"));
+});
